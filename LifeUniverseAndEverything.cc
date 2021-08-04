4d4a22ffde47a52672be164f73936e9fa2bbb7e3
MD. Mehedi Hasan <53508562+MehediMubin@users.noreply.github.com>
2021-08-04 12:33:08 +0600

    Create LifeUniverseAndEverything.cc

---
+++ LifeUniverseAndEverything.cc
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define nl "\n"
+#define ll long long
+#define pb push_back
+#define all(v) (v).begin(),(v).end()
+#define what_is(x) cerr << #x << " is " << x << endl;
+#define FastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+int main ()
+{
+    FastIO;
+
+    int n;
+    while (cin >> n) {
+        if (n == 42) break;
+        cout << n << nl;
+    }
+
+    return 0;
+}
